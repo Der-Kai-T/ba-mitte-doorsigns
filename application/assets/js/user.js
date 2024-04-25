bd20b5c2c2cd07384fc5e60732bde2fc1238cb79
Kai <git@kai-thater.de>
2024-04-25 16:04:56 +0200

    V1.1: UI improvements
    made editing easier

--- application/assets/js/user.js
+++ application/assets/js/user.js
@@ -13,7 +13,8 @@ let line_2_2 ;
 let line_2_3 ;
 let line_2_4 ;
 let line_2_5 ;
-
+let table;
+let data = [];
 $(document).ready(function(){
      authority = $('#authority');
      room = $('#room');
@@ -30,6 +31,8 @@ $(document).ready(function(){
      line_2_3 = $('#line_2_3');
      line_2_4 = $('#line_2_4');
      line_2_5 = $('#line_2_5');
+     table = $('#datatable');
+     load_browser();
 });
 function toggle_card_body(card_, caret_) {
     let card = $('#' + card_);
@@ -47,21 +50,58 @@ function toggle_card_body(card_, caret_) {
 }
 
 
-let data = [];
+
 
 
 function clear_all() {
-    $('#json').val("");
+    //clear array
+    data = [];
+    localStorage.removeItem("json");
+    update_json();
+    generate_table();
 }
 
 function clear_inputs() {
 
+    //clear these fields only if checkbox is not set
+    console.log("Authority");
+    console.log($('#authority_checkbox').is(':checked'));
+    if($('#authority_checkbox').is(':checked') === false){
+        authority.val("");
+    }
 
-    authority.val("");
-    room.val("");
-    department.val("");
-    section_number.val("");
-    section_name.val("");
+
+    console.log("Departement");
+    console.log($('#department_checkbox').is(':checked'));
+    if($('#department_checkbox').is(':checked') === false){
+        department.val("");
+    }
+
+    console.log("Section Number");
+    console.log($('#section_number_checkbox').is(':checked'));
+    if($('#section_number_checkbox').is(':checked') === false){
+        section_number.val("");
+    }
+
+    console.log("Room");
+    console.log($('#room_checkbox').is(':checked'));
+    if($('#room_checkbox').is(':checked') === false){
+        room.val("");
+    }
+
+    console.log("Section Name");
+    console.log($('#section_name_checkbox').is(':checked'));
+    if($('#section_name_checkbox').is(':checked') === false){
+        section_name.val("");
+    }
+
+
+
+
+
+
+
+
     line_1_1.val("");
     line_1_2.val("");
     line_1_3.val("");
@@ -96,24 +136,111 @@ function add_data() {
 
     data.push(entry)
 
-    $('#json').val(JSON.stringify(data));
     console.log(data);
+    update_json();
+    generate_table();
     clear_inputs();
 }
 
-function safe_browser(){
-    localStorage.setItem("json", JSON.stringify(data));
-    console.log(localStorage.getItem("json"));
+function generate_table(){
+
+    $('#datatable tbody').html (" ");
+    for(let i = 0; i < data.length; i ++){
+        let entry = data[i];
+        let names = "";
+
+        if(entry.line_1_1.length > 0){
+            names += "1.1: " + entry.line_1_1 + "<br>";
+        }
+        if(entry.line_1_2.length > 0){
+            names += "1.2: " + entry.line_1_2 + "<br>";
+        }
+        if(entry.line_1_3.length > 0){
+            names += "1.3: " + entry.line_1_3 + "<br>";
+        }
+        if(entry.line_1_4.length > 0){
+            names += "1.4: " + entry.line_1_4 + "<br>";
+        }
+        if(entry.line_1_5.length > 0){
+            names += "1.5: " + entry.line_1_5 + "<br>";
+        }
+
+        if(entry.line_2_1.length > 0){
+            names += "2.1: " + entry.line_2_1 + "<br>";
+        }
+        if(entry.line_2_2.length > 0){
+            names += "2.2: " + entry.line_2_2 + "<br>";
+        }
+        if(entry.line_2_3.length > 0){
+            names += "2.3: " + entry.line_2_3 + "<br>";
+        }
+        if(entry.line_2_4.length > 0){
+            names += "2.4: " + entry.line_2_4 + "<br>";
+        }
+        if(entry.line_2_5.length > 0){
+            names += "2.5: " + entry.line_2_5 + "<br>";
+        }
+
+        let department_string = entry.authority + "<br>" +
+            entry.department + "<br>" +
+            entry.section_number + "<br>" +
+            entry.section_name + "<br>"
+        ;
+
+
+        let tr = "<tr id='row_index_"+i+"'>" +
+            "<td>" +
+            entry.room +
+            "</td>" +
+            "<td>" +
+            department_string +
+            "</td>" +
+            "<td>" +
+            names +
+            "</td>" +
+            "<td> <span class='btn' onclick='remove_entry(" + i + ")'><span class='fas fa-trash'</span></td>" +
+        "</tr>";
+
+    table.append(tr);
+    }
+}
+function update_json(){
+    let json_string = JSON.stringify(data);
+
+    //store data in Browserstorage
+    localStorage.setItem("json", json_string);
+
+    //update textbox for export
+    $('#json').val(json_string);
+}
+function remove_entry(entry_index){
+    //remove item from array
+    data.splice(entry_index, 1);
+
+    //regenerate strings
+    update_json();
+
+    //regenerate displayed table;
+    generate_table();
 }
 
+
+
 function load_browser(){
+    //load json from storage
     data = JSON.parse(localStorage.getItem("json"));
+
+    //if emtpy create empty string
     if(data === null){
         data = [];
     }
+
+    //write string to textbox
     $('#json').val(JSON.stringify(data));
+
+    generate_table();
 }
 
 function clear_browser(){
-    localStorage.removeItem("json");
+
 }
--- application/index.php
+++ application/index.php
@@ -25,41 +25,14 @@
 <body>
 <div class="container mt-3">
     <h2>Türschilder</h2>
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Informationen <span class="fa-solid fa-caret-down pointer" id="info_caret"
-                                                               onclick="toggle_card_body('info_body', 'info_caret')"></span>
-                    </h3>
-                </div>
-                <div class="card-body" id="info_body" style="display:none">
-                    <p>Mithilfe dieser Webanwendung können Türschilder für die Büros im (angemieteten) D-Flur des
-                        Bezirksamtsgebäudes in der Caffamacherreihe 1-3 erstellt werden.</p>
 
-                    <p>
-                        Aktuell steht nur das Logo des Bezirksamtes zur Verfügung. Weitere Logos können an
-                        <a href="mailto:dev@kai-thater.de?subject=Logo%20Türschilder">dev@kai-thater.de</a>
-                        gesendet werden.
-
-                    </p>
-                    <p>Anforderungen an die Datei</p>
-                    <ul>
-                        <li> Dateiformat .png mit transparentem Hintergrund</li>
-                        <li> Auflösung 300dpi</li>
-                        <li> Maße: 70mm breit, 25mm hoch (inkl. Logoschutzzonen)</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
     <div class="row mt-3">
-        <div class="col-12">
+        <div class="col-6">
             <div class="card">
                 <div class="card-header">
-                    Daten
+                    Daten Eingabe
                 </div>
                 <div class="card-body">
 
@@ -82,7 +55,7 @@
 
                     <div class="row">
                         <div class="col-12">
-                            <?= input("department", "Fachamt", true); ?>
+                            <?= input("department", "Fachamt (inkl. des Wortes Fachamt, wenn gewünscht)", true); ?>
                         </div>
                     </div>
 
@@ -132,66 +105,74 @@
                                         class="fa-solid fa-save"></span><span class="p-3">Eintrag speichern</span>
                             </button>
                             <button class="btn btn-danger mt-3" onclick="clear_inputs()"><span
-                                        class="fa-solid fa-trash"></span><span class="p-3">Eingaben leeren</span>
+                                        class="fa-solid fa-trash"></span><span class="p-3">Felder leeren</span>
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
+        <div class="col-6">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Erfasste Daten
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-striped" id="datatable">
+                                <thead>
+                                <tr>
+                                    <th>Raum</th>
+                                    <th>Fachamt / Abteilung</th>
+                                    <th>Personen</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
 
-    <div class="row mt-3">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    Daten weiterverarbeiten
-                </div>
-                <div class="card-body">
-                    <p>
-                        Hier werden die Daten, die oben eingegeben werden in einem maschinenlesbaren Format (json)
-                        abgelegt.
-                        Wenn Sie Ihre Arbeit teilen möchten, speichern sie den Inhalt des Feldes mit Hilfe eines
-                        Text-Editor (z.B. Notepad unter Windows) als .txt-Datei und geben Sie diese weiter. Verwenden
-                        Sie kein Textverarbeitungsprogramm wie
-                        Microsoft Word, da dieses ggf. den Inhalt verändert und Sie so alle Daten verlieren!
-                    </p>
-                    <p>Zum laden der Daten kopieren Sie diese in das Textfeld.</p>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-footer">
+                            <form action="generate.php" method="POST">
+                                <textarea name="json" id="json" class="form-control" style="font-family: monospace"></textarea>
 
-                    <p>Achtung! Wenn Sie die Daten über diese Möglichkeit austauschen können Sie anschließend keine
-                        weiteren
-                        Daten über das Formular oben eingeben, da dies Ihre kopierten Daten überschreiben würde.</p>
+                                <button type="submit" class="btn btn-primary mt-3"><span
+                                            class="fa-solid fa-file-pdf"></span><span class="p-3">PDF erzeugen</span></button>
 
-                    <p>Verwenden Sie zum Speichern daher die Buttons unten, die die Daten in ihrem lokalen
-                        Browserstorage speichern.</p>
+                            </form>
+                            <button class="btn btn-danger mt-1" onclick="clear_all()"><span
+                                        class="fa-solid fa-trash"></span><span class="p-3">alle Daten löschen</span></button>
+                        </div>
 
-                    </p>
-                    <p>Wenn Sie mit Ihrer Arbeit fertig sind, klicken Sie auf <span class="fa-solid fa-file-pdf"></span><span
-                                class="p-1">PDF erzeugen</span></p>
-
-
-                    <form action="generate.php" method="POST">
-                        <textarea name="json" id="json" class="form-control" style="font-family: monospace"></textarea>
-
-                        <button type="submit" class="btn btn-primary mt-3"><span
-                                    class="fa-solid fa-file-pdf"></span><span class="p-3">PDF erzeugen</span></button>
-
-                    </form>
-                    <button class="btn btn-danger mt-1" onclick="clear_all()"><span
-                                class="fa-solid fa-trash"></span><span class="p-3">Daten leeren</span></button>
-
-                    <h4 class="mt-3">Speichern im lokalen Browserstorage</h4>
-                    <button class="btn btn-info mt-3" onclick="safe_browser()"><span
-                                class="fa-solid fa-save"></span><span class="p-3">Daten speichern</span></button>
-                    <button class="btn btn-info mt-3" onclick="load_browser()"><span
-                                class="fa-solid fa-folder-open"></span><span class="p-3">Daten laden</span></button>
-                    <button class="btn btn-danger mt-3" onclick="clear_browser()"><span
-                                class="fa-solid fa-trash"></span><span class="p-3">Daten löschen</span></button>
+                    </div>
                 </div>
             </div>
+
+            <div class="row mt-2">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Feldinhalt nicht leeren
+                        </div>
+                        <div class="card-body">
+                            <input type="checkbox" class="form-check-input mr-2" id="authority_checkbox" checked><label for="room_checkbox" class="form-check-label ml-2">Behörde</label><br>
+                            <input type="checkbox" class="form-check-input mr-2" id="room_checkbox"><label for="room_checkbox" class="form-check-label ml-2">Raum</label><br>
+                            <input type="checkbox" class="form-check-input mr-2" id="department_checkbox" checked><label for="room_checkbox" class="form-check-label ml-2">Fachamt</label><br>
+                            <input type="checkbox" class="form-check-input mr-2" id="section_number_checkbox"><label for="room_checkbox" class="form-check-label ml-2">Leitzeichen Abteilung / Abschnitt</label><br>
+                            <input type="checkbox" class="form-check-input mr-2" id="section_name_checkbox"><label for="room_checkbox" class="form-check-label ml-2">Name Abteilung / Abschnitt</label><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
+
+
+
 
 
     <div class="row mt-3">
@@ -205,6 +186,35 @@
                          class="img-fluid"
                          alt="Darstellung der Felder">
 
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Informationen <span class="fa-solid fa-caret-down pointer" id="info_caret"
+                                                               onclick="toggle_card_body('info_body', 'info_caret')"></span>
+                    </h3>
+                </div>
+                <div class="card-body" id="info_body" style="display:none">
+                    <p>Mithilfe dieser Webanwendung können Türschilder für die Büros im (angemieteten) D-Flur des
+                        Bezirksamtsgebäudes in der Caffamacherreihe 1-3 erstellt werden.</p>
+
+                    <p>
+                        Aktuell steht nur das Logo des Bezirksamtes zur Verfügung. Weitere Logos können an
+                        <a href="mailto:dev@kai-thater.de?subject=Logo%20Türschilder">dev@kai-thater.de</a>
+                        gesendet werden.
+
+                    </p>
+                    <p>Anforderungen an die Datei</p>
+                    <ul>
+                        <li> Dateiformat .png mit transparentem Hintergrund</li>
+                        <li> Auflösung 300dpi</li>
+                        <li> Maße: 70mm breit, 25mm hoch (inkl. Logoschutzzonen)</li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -235,15 +245,14 @@
                     </p>
 
                     <p>
-                        Die von Ihnen über den "Speichern"-Button gespeicherten Daten werden in Ihrem lokalen Browser-Storage
-                        gespeichert und nicht an den Server übertragen.
+                        Die Speicherung der Daten erfolgt im lokalen Browserstorage.
                     </p>
 
 
 
                 </div>
                 <div class="card-footer">
-                    Version 1.0 | Das PDF-Skript wurde
+                    Version 1.1 | Das PDF-Skript wurde
                     <?php
                     $number = file_get_contents("counter.txt");
                     echo $number;
